939a90cf39d1fddef0134cfe002bf11e995b2a6c
LeviMoore <mail@levimoore.dk>
2020-09-27 18:00:26 +0200

    added delete button to uploaded file list

---
+++ packages/book-hub/api/add/delete.php
@@ -0,0 +1,39 @@
+<?php
+
+include($_SERVER['DOCUMENT_ROOT'] . "/framework.php");
+Functions::collect();
+
+if (Packages::exist("authentication"))
+{
+    Authentication::Auth403();
+}
+
+$uploadFolder = Packages::serverPath("book-hub") . "/uploads/";
+
+if (isset($_POST["name"]))
+{
+    $fileName = $_POST["name"];
+    $fileName = Helper::escapeFileName($fileName);
+    $finalPath = $uploadFolder . $fileName;
+
+    if (file_exists($finalPath))
+    {
+        chmod($finalPath, 0777);
+        unlink($finalPath);
+        $return["result"]["success"] = true;
+        $return["result"]["message"] = "File removed";
+    }
+    else
+    {
+        $return["result"]["success"] = false;
+        $return["result"]["message"] = "Cant find file";
+    }
+}
+else
+{
+    $return["result"]["success"] = false;
+    $return["result"]["message"] = "Name data is missing";
+}
+
+$return["status"] = "OK";
+exit(json_encode($return));
--- packages/book-hub/pages/add/code.js
+++ packages/book-hub/pages/add/code.js
@@ -1,137 +1,187 @@
-function scan()
-{
-    console.log(1);
-}
-
 $(document).ready(function()
 {
     $("#form").on('submit', function(e)
     {
         e.preventDefault();
-
-        var progress = $("#progress-bar");
-        var files = $('#files').get(0).files;
-        var count = files.length;
-
-        //check if no files are selected
-        if (count === 0)
-        {
-            Alert.error("No file selected.");
-            return;
-        }
-
-        //prepare data
-        var data = new FormData();
-        for (var i = 0; i < count; i++)
-        {
-            data.append("files[]", files[i]);
-        }
-        
-        //call upload api
-        API.upload("book-hub", "add/upload", data, progress,
-            function(result)
-            {
-                //if successful
-                if (result["success"] == true)
-                {
-                    Alert.success(result["message"]);
-                    
-                    //add uploaded files to table
-                    result["uploaded"].forEach(item =>
-                    {
-                        addUploadedFileToList(item);
-                    });
-                    sortUploadedFilesList();
-
-                    enableForm();
-                    clearForm();
-                }
-                //if unsuccessful
-                else if (result["success"] == false)
-                {
-                    Alert.error(result["message"]);
-                    enableForm();
-                    clearForm();
-                }
-            },
-            function(result) //failed
-            {
-                Alert.error("Something went wrong. See console (F12) for more info.");
-                enableForm();
-                clearForm();
-            }
-        );
-
-        disableForm();
+        submit();
     });
 
     $('#files').on('change', function()
     {
-        var files = $('#files').get(0).files;
-        var count = files.length;
-
-        //check for unsupported files
-        var supportedFormat = ['pdf'];
-        for (var i = 0; i < count; i++)
-        {
-            var type = files[i]["name"].split('.').pop();
-            if ($.inArray(type, supportedFormat) < 0)
-            {
-                Alert.error("Format " + type + " not supported. Use " + supportedFormat.join(', '));
-                clearForm();
-                return;
-            }
-        }
-
-        //file size
-        var totalSize = 0;
-        var maxMB = 100;
-        for (var i = 0; i < count; i++)
-        {
-            totalSize += files[i]["size"];
-        }
-        if(totalSize > 1048576 * maxMB)
-        {
-            Alert.error("Files are too big. Can only upload a combined " + maxMB + "MB");
-            clearForm();
-        }
+        filesSelectUpdate();
     });
 
-    function addUploadedFileToList(name)
-    {
-        $("#uploadedFiles tbody").append("<tr><td>" + name.split('\\').pop() + "</td></tr>");
-    }
-    function sortUploadedFilesList()
-    {
-        var tbody = $("#uploadedFiles tbody");
-        tbody.find('tr').sort(function(a, b)
-        {
-            return $('td:first', a).text().localeCompare($('td:first', b).text());
-        }).appendTo(tbody);
-    }
+    ready();
+});
 
-    function disableForm()
-    {
-        $('#submit').attr("disabled", "disabled");
-        $('#files').attr("disabled", "disabled");
-    }
-    function enableForm()
-    {
-        $('#submit').removeAttr("disabled");
-        $('#files').removeAttr("disabled");
-    }
-    function clearForm()
-    {
-        $('#files').val(null);
-    }
-
+function ready()
+{
     //reset progress bar
     $("#progress-bar").width('0%');
     $("#progress-bar").html('0%');
 
     //add already uploaded files to list
-    uploadedServerList.forEach(item => {
+    uploadedServerList.forEach(item =>
+    {
         addUploadedFileToList(item);
     });
     sortUploadedFilesList();
-});
+}
+
+function submit()
+{
+    var progress = $("#progress-bar");
+    var files = $('#files').get(0).files;
+    var count = files.length;
+
+    //check if no files are selected
+    if (count === 0)
+    {
+        Alert.error("No file selected.");
+        return;
+    }
+
+    //prepare data
+    var data = new FormData();
+    for (var i = 0; i < count; i++)
+    {
+        data.append("files[]", files[i]);
+    }
+
+    //call upload api
+    API.upload("book-hub", "add/upload", data, progress,
+        function(result)
+        {
+            //if successful
+            if (result["success"] == true)
+            {
+                Alert.success(result["message"]);
+
+                //add uploaded files to table
+                result["uploaded"].forEach(item =>
+                {
+                    addUploadedFileToList(item);
+                });
+                sortUploadedFilesList();
+
+                enableForm();
+                clearForm();
+            }
+            //if unsuccessful
+            else if (result["success"] == false)
+            {
+                Alert.error(result["message"]);
+                enableForm();
+                clearForm();
+            }
+        },
+        function(result) //failed
+        {
+            Alert.error("Something went wrong. See console (F12) for more info.");
+            enableForm();
+            clearForm();
+        }
+    );
+
+    disableForm();
+}
+
+function filesSelectUpdate()
+{
+    var files = $('#files').get(0).files;
+    var count = files.length;
+
+    //check for unsupported files
+    var supportedFormat = ['pdf'];
+    for (var i = 0; i < count; i++)
+    {
+        var type = files[i]["name"].split('.').pop();
+        if ($.inArray(type, supportedFormat) < 0)
+        {
+            Alert.error("Format " + type + " not supported. Use " + supportedFormat.join(', '));
+            clearForm();
+            return;
+        }
+    }
+
+    //file size
+    var totalSize = 0;
+    var maxMB = 100;
+    for (var i = 0; i < count; i++)
+    {
+        totalSize += files[i]["size"];
+    }
+    if (totalSize > 1048576 * maxMB)
+    {
+        Alert.error("Files are too big. Can only upload a combined " + maxMB + "MB");
+        clearForm();
+    }
+}
+
+function addUploadedFileToList(name)
+{
+    $("#uploadedFiles tbody").append(`
+    <tr>
+        <td>
+            ${name.split('\\').pop()}
+        </td>
+        <td>
+            <button class="btn btn-danger" onClick="remove(this, '${name}')">
+                <i class="fas fa-trash-alt"></i>
+            </button>
+        </td>
+    </tr>
+    `);
+}
+
+function sortUploadedFilesList()
+{
+    var tbody = $("#uploadedFiles tbody");
+    tbody.find('tr').sort(function(a, b)
+    {
+        return $('td:first', a).text().localeCompare($('td:first', b).text());
+    }).appendTo(tbody);
+}
+
+function disableForm()
+{
+    $('#submit').attr("disabled", "disabled");
+    $('#files').attr("disabled", "disabled");
+}
+
+function enableForm()
+{
+    $('#submit').removeAttr("disabled");
+    $('#files').removeAttr("disabled");
+}
+
+function clearForm()
+{
+    $('#files').val(null);
+}
+
+function remove(listElement, name)
+{
+    var data = {
+        name: name
+    };
+    API.simple("book-hub", "add/delete", data,
+        function (result)
+        {
+            if (result["success"] == true)
+            {
+                Alert.success(result["message"]);
+                $(listElement).parent().parent().remove();
+            }
+            else if (result["success"] == false)
+            {
+                Alert.error(result["message"]);
+            }
+        },
+        function (result)
+        {
+            Alert.error("Something went wrong. See console (F12) for more info.");
+            Alert.error(result["message"]);
+        }
+    );
+}
