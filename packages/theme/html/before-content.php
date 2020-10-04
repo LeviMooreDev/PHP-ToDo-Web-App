58b8a46ffe28d6b1084fe1c6e52f5e0e4b7f1c7d
LeviMoore <mail@levimoore.dk>
2020-10-04 16:45:55 +0200

    menu site name links to home page

--- packages/theme/html/before-content.php
+++ packages/theme/html/before-content.php
@@ -23,7 +23,7 @@ usort($menuItems, function($a, $b) {
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#"><?=Info::Name?></a>
+    <a class="navbar-brand" href="/"><?=Info::Name?></a>
 
     <div class="collapse navbar-collapse" id="navbarToggler">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
