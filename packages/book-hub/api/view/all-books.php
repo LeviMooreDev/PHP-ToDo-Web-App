ee4ce8895f4b79d6a3b0633ae48071503c88e302
LeviMoore <mail@levimoore.dk>
2020-10-08 23:02:04 +0200

    description is no longer returned

--- packages/book-hub/api/view/all-books.php
+++ packages/book-hub/api/view/all-books.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 include("../core.php");
 
 Database::connect();
-$result = Database::query("SELECT *, created_timestamp as added FROM `book-hub`");
+$result = Database::query("SELECT `id`, `title`, `subtitle`, `categories`, `authors`, `publisher`, `date`, `page`, `status`, `isbn13`, `isbn10`, `cover-color`, `update_timestamp`, `created_timestamp` as added FROM `book-hub`");
 
 $books = [];
 if ($result->num_rows > 0)
