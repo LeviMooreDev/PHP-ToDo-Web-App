56d0e9541b5d4ee2a08916f44401f9fa02285a38
LeviMoore <mail@levimoore.dk>
2021-03-30 21:28:48 +0200

    removed book hub

--- packages/book-hub/pages/list/head.php
+++
@@ -1,2 +0,0 @@
-<?php
-BootstrapSelect::AddCSS();
--- packages/book-hub/pages/list/scripts.php
+++
@@ -1,5 +0,0 @@
-<?php BootstrapSelect::AddScript(); ?>
-<?php Asuggest::AddScript(); ?>
-<script>
-var coverQuality = "<?=str_replace("%","",UserSettings::get("book-hub", "cover-quality", "20%"));?>"; 
-</script>
