ce38551a95161b2e1451a2da2af9cc42fa650473
LeviMoore <mail@levimoore.dk>
2021-04-06 01:36:49 +0200

    fixed deletion not triggering auto-refresh if the first task in the table is ones own

--- packages/todo/api/delete.php
+++ packages/todo/api/delete.php
@@ -29,7 +29,7 @@ Database::query("DELETE FROM `$table` WHERE `id`=$id");
 //this is a nasty hack to get auto refresh to work.
 //when deleting a task we update another task which will trigger a refresh.
 //need a better way to auto refresh when deleting.
-Database::query("UPDATE `$table` SET `updated_at` = CURRENT_TIMESTAMP LIMIT 1;");
+Database::query("UPDATE `$table` SET `updated_at` = CURRENT_TIMESTAMP, `updated_by`='server' LIMIT 1;");
 
 //return
 API::success("Deleted");
